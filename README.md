631eca5da44f5d6f2c0ff12eb4d249c2add2f80a
Kyxxn <gywns626@naver.com>
2024-10-08 00:40:16 +0900

    chore: 프로젝트 세팅 & 리드미 추가
    이번 프로젝트가 UIKit으로 되어 있는데, Feature는 스유로 작업해야 함
    즉, UIKit 베이스에 스유를 띄우며 공부할 예정

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SwiftUI-Study
 언젠가 할 거 같았는데, 이번에 하게 된 김에 레포까지
+
+UIKit 베이스에서 SwiftUI를 띄우며 진행합니다.  
---
+++ SwiftUI-Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUI-Study
+//
+//  Created by 박효준 on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
