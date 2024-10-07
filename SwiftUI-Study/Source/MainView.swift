8318c58be0a7cc24bf0ead8d7d6dc9ddb60d1781
Kyxxn <gywns626@naver.com>
2024-10-08 01:00:31 +0900

    feat: SwiftUI 연결

---
+++ SwiftUI-Study/Source/MainView.swift
@@ -0,0 +1,14 @@
+//
+//  MainView.swift
+//  SwiftUI-Study
+//
+//  Created by 박효준 on 10/8/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ SwiftUI-Study/Source/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  SwiftUI-Study
+//
+//  Created by 박효준 on 10/8/24.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UIHostingController(rootView: MainView())
+        window?.makeKeyAndVisible()
+    }
+}
+
